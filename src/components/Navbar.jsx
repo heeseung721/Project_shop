b34cc9a35e02baa5681a16bbf1bfb69d1052cd22
heeseung lee <heeseung721@gmail.com>
2023-02-26 22:56:25 +0900

    firebase 인증을 활용한 로그인 기능 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  // 컴포넌트가 처음 마운트 되었을 때 딱 한번만 함수를 호출해서
+  // 유저를 전달받으면 유저의 상태가 변경 될 때마다 setUser 설정(로그인 or 로그아웃 상태)
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+      console.log(user);
+    });
+  }, []);
+
+  //로그인이 성공하면 setUser로 유저를 설정함
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <>
       <header className={styles.header}>
@@ -14,7 +35,9 @@ export default function Navbar() {
           <Link to="/products">Products</Link>
           <Link to="/carts">Carts</Link>
           <Link to="/products/new">NEW</Link>
-          <button>Login</button>
+
+          {!user && <button onClick={handleLogin}>Login</button>}
+          {user && <button onClick={handleLogout}>Logout</button>}
         </nav>
       </header>
     </>
